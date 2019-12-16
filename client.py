d04e29e49b01feee7b8dcce1659e647f88907e1d
otavioalbucosta <otavio-albucosta@hotmail.com>
2019-12-15 21:48:11 -0300

    yee

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+import pickle
+
 MAXBYTES = 4056
 def show_board(board):
     tabuleiro = '''
@@ -25,12 +27,13 @@ res = s.recv(MAXBYTES).decode("utf8")
 print(res)
 if res == "você irá iniciar":
     while True:
-        res = s.recv(MAXBYTES).decode("utf8")
+        res = pickle.loads(s.recv(MAXBYTES))
         if res == "end":
             break;
         else:
             print(show_board(res))
-            value = input("Digite a Letra e o número")
+            value = input("Digite a Letra (maiúscula) e o número")
             if res[value[0]][value[1]] != None or ord(value[0]) < ord("A") or ord(value[0]) > ord("C") or int(value[1]) < 1 or int(value[1]) > 3 :
                 print("Essa casa está ocupada ou você digitou alguma letra ou numero inexistente");
-            s.send(value.upper().encode("utf8"))
+            else:
+                s.send(pickle.dumps(value.upper()))
--- server.py
+++ server.py
@@ -1,14 +1,67 @@
 import socket
 import random
+import pickle
 MAXBYTES = 4056
+def check_game_win_condition(board):
+    if board["A"]["1"] == "X" and board["B"]["2"] == "X" and board["C"]["3"] == "X" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "X"
+    elif board["A"]["3"] == "X" and board["B"]["2"] == "X" and board["C"]["1"] == "X" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "X"
+    elif board["A"]["1"] == "X" and board["A"]["2"] == "X" and board["A"]["3"] == "X" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "X"
+    elif board["B"]["1"] == "X" and board["B"]["2"] == "X" and board["B"]["3"] == "X" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "X"
+    elif board["C"]["1"] == "X" and board["C"]["2"] == "X" and board["C"]["3"] == "X" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "X"
+    elif board["A"]["1"] == "X" and board["B"]["1"] == "X" and board["C"]["1"] == "X" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "X"
+    elif board["A"]["2"] == "X" and board["B"]["2"] == "X" and board["C"]["2"] == "X" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "X"
+    elif board["A"]["3"] == "X" and board["B"]["3"] == "X" and board["C"]["3"] == "X" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "X"
+    elif board["A"]["1"] == "O" and board["B"]["2"] == "O" and board["C"]["3"] == "O" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "O"
+    elif board["A"]["3"] == "O" and board["B"]["2"] == "O" and board["C"]["1"] == "O" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "O"
+    elif board["A"]["1"] == "O" and board["A"]["2"] == "O" and board["A"]["3"] == "O" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "O"
+    elif board["B"]["1"] == "O" and board["B"]["2"] == "O" and board["B"]["3"] == "O" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "O"
+    elif board["C"]["1"] == "O" and board["C"]["2"] == "O" and board["C"]["3"] == "O" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "O"
+    elif board["A"]["1"] == "O" and board["B"]["1"] == "O" and board["C"]["1"] == "O" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "O"
+    elif board["A"]["2"] == "O" and board["B"]["2"] == "O" and board["C"]["2"] == "O" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "O"
+    elif board["A"]["3"] == "O" and board["B"]["3"] == "O" and board["C"]["3"] == "O" :
+        print("TEMOS UM VENCEDOR!!!")
+        return "O"
+    elif  board["A"]["3"] != None and board["B"]["3"] != None and board["C"]["3"] != None and board["A"]["2"] != None and board["B"]["2"] != None and board["C"]["2"] != None  and board["A"]["1"] != None and board["B"]["1"] != None and board["C"]["1"] != None:
+        print("TEMOS UM VENCEDOR!!!")
+        return "D"
+    else: 
+        return "N"
 
 def start_gaming(socket1,socket2):
-    while True:
-        board = {"A": {"1": None, "2":None, "3":None}, 
+    board = {   "A": {"1": None, "2":None, "3":None}, 
                 "B": {"1": None, "2":None, "3":None},
                 "C": {"1": None, "2":None, "3":None}}
-        
-        
+    while True:
         sock = random.randrange(1,2)
         if sock == 1: 
             socket1.send("você irá iniciar".encode("utf8"))
@@ -17,14 +70,15 @@ def start_gaming(socket1,socket2):
             socket2.send("você irá iniciar".encode("utf8"))
             socket1.send("o adversário iniciará".encode('utf8'))
         if sock == 1:
-            while true:
+            while True:
                 socket1.send(board)
-                res = socket1.recv(MAXBYTES).decode("utf8")
-                board[res[0]][res[1]] = X
+                res = pickle.loads(socket1.recv(MAXBYTES))
+                board[res[0]][res[1]] = "X"
+                check_game_win_condition(board)
                 
 
-serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+serversocket1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 5656
-serversocket.bind((host, port))
-serversocket.listen(10)
+serversocket1.bind((host, port))
+serversocket1.listen(10)
